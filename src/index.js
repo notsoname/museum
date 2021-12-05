3f710bf6de0bac591f5ca86e5decc3c78600b9b5
Asar Akhmetbekov <you@example.com>
2021-12-05 14:07:44 +0600

    g

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './sass/index.scss';
-
-import * as videoJs from './video'
-import * as gallery from './gallery'
-import * as tickets from './tickets'
-import * as welcome from './welcome'
-import * as explore from './explore'
